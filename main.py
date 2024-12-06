66036f6c0fd3e7a03c4b289519299a25c9403d22
Alex <alemskx@gmail.com>
2024-12-06 10:15:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
-import json
-num = input("Введите номер квалификации: ")
-find = False
-with open("dump.json", 'r', encoding='utf-8') as file: 
-    source_file = json.load(file)
-    #Квалификация 
-    for skill in source_file:
-        if skill.get("model") == "data.skill" and skill.get("fields").get("code")==num:
-            skill_code = skill.get("fields").get("code")
-            skill_title =  skill_title = skill.get("fields").get("title")
-            find = True
-            #Cпециальность
-            for profes in source_file:
-                if profes.get("model") == "data.specialty":
-                    if profes.get("fields").get("code") in num:
+import json  
+num = str(input("Введите номер квалификации для поиска: "))
+skills = False
+
+with open('dump.json', 'r', encoding='utf-8') as file: 
+    data = json.load(file) 
+    #Квалификация
+    for skill in data:
+        if skill.get("model") == "data.skill":
+            if skill["fields"].get("code") == num: 
+                skill_code = skill["fields"].get("code")
+                skill_title = skill["fields"].get("title")
+                skill_specialty =  skill["fields"].get("specialty")
+                skills = True
+                #Специальность
+                for profes in data:
+                    if profes.get("model") == "data.specialty":
                         profes_code = profes.get("fields").get("code")
-                        profes_title = profes.get("fields").get("title")
-                        profes_type = profes.get("fields").get("c_type")
-            break    
-#Вывод        
-if not find:
-    print("=" * 20, "Не найдено", "=" * 20)
+                        profes_pk = profes["pk"]
+                        if profes_code in num and  skill_specialty == profes_pk :  
+                            profes_title = profes["fields"].get("title")
+                            profes_type = profes["fields"].get("c_type")
+                            break    
+                break  
+
+
+#Вывод
+if not skills:
+    print("=============== Не Найдено ===============") 
 else:
-    print(f"{skill_code} >> Квалификация '{skill_title}'")
-    print(f"{profes_code} >> Специальность '{profes_title}', {profes_type}")
+    print("=============== Найдено ===============") 
+    print(f"{skill_code} >> Квалификация {skill_title}")
+    print(f"{profes_code} >> Специальность {profes_title} , {profes_type}")
+    
